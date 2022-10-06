a0167abf94c9b365cf242148ed6c86c908e1f6eb
mehtachirag186 <97532641+mehtachirag186@users.noreply.github.com>
2022-10-06 21:12:45 +0530

    update

--- backend.tf
+++ backend.tf
@@ -5,6 +5,10 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+  subscription_id = "a0d0daf4-8e05-41ba-add7-e0856bac7f78"
+  tenant_id       = "0cb6f4c9-1ea8-4af8-9982-6a357695bb22"
+  client_id       = "9ed9ec93-681e-47d4-b5dd-cd2a2fcba037"
+  client_secret   = "RLh8Q~7nhdXgJQHzm8RQqcfvgh3Yfej83JJg7djY"
 }
 
 terraform {
